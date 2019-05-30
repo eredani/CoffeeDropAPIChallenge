87f0190b0478a57c02a2f634f1731a9d03d4dada
eRe <danieremihai@gmail.com>
2019-05-31 00:52:47 +0100

    Added the Path to the Seeder

---
+++ Laravel_API/database/seeds/Locations.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Locations extends Seeder
+{
+        /**
+     * Path to the CSV file with all Coffee Drop informations.
+     *
+     * @var string
+     */
+    protected $PATH = '../location_data.csv';
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
