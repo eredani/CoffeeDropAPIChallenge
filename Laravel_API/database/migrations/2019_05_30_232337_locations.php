028e5aec69ce520c7aef559356d59f8ff93a711b
eRe <danieremihai@gmail.com>
2019-05-31 00:46:44 +0100

    Install and Migrations

---
+++ Laravel_API/database/migrations/2019_05_30_232337_locations.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Locations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function ( Blueprint $table ) {
+            $table->increments('id');
+            $table->string('postCode');
+            $table->decimal('lat', 11, 8 )->nullable();
+            $table->decimal('lng', 11, 8 )->nullable();
+            $table->json('timeTable');
+            $table->timestamps();
+        } );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations' );
+    }
+}
